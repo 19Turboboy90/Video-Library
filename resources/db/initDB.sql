6213c3fcd159761a8942a436d65cd4c467a9830d
Ilia Zharinov <19turboboy90@gmail.com>
2023-09-05 17:58:12 +0300

    Update DATA BASE

--- resources/db/initDB.sql
+++ resources/db/initDB.sql
@@ -1,10 +1,24 @@
 DROP TABLE feedback;
+DROP TABLE users;
+DROP TABLE actor_movie;
 DROP TABLE actor;
 DROP TABLE movie;
+DROP TABLE genre;
 DROP TABLE director;
-DROP TABLE users;
+DROP TABLE country;
 
 
+CREATE TABLE country
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(150)
+);
+
+CREATE TABLE genre
+(
+    id    SERIAL PRIMARY KEY,
+    title VARCHAR(50)
+);
 
 CREATE TABLE director
 (
@@ -25,10 +39,10 @@ CREATE TABLE movie
 (
     id            SERIAL PRIMARY KEY,
     name          VARCHAR(256) NOT NULL,
-    director_id   INTEGER REFERENCES director (id) ON DELETE CASCADE,
+    director_id   INTEGER REFERENCES director (id),
     premiere_date DATE         NOT NULL,
-    country       VARCHAR(128) NOT NULL,
-    genre         VARCHAR(128) NOT NULL
+    country_id    INTEGER REFERENCES country (id),
+    genre_id      INTEGER REFERENCES genre (id)
 );
 
 CREATE TABLE actor
@@ -39,11 +53,18 @@ CREATE TABLE actor
     movie_id      INTEGER REFERENCES movie (id) ON DELETE CASCADE
 );
 
+CREATE TABLE actor_movie
+(
+    id       SERIAL PRIMARY KEY,
+    actor_id INTEGER REFERENCES actor (id) ON DELETE CASCADE,
+    movie_id INTEGER REFERENCES movie (id) ON DELETE CASCADE
+);
+
 CREATE TABLE feedback
 (
     id         SERIAL PRIMARY KEY,
     movie_id   INT REFERENCES movie (id) ON DELETE CASCADE,
     user_id    INT REFERENCES users (id) ON DELETE CASCADE,
     text       VARCHAR(1024),
-    assessment INTEGER CHECK ( assessment >= 0 AND assessment <= 10) NOT NULL
+    assessment INTEGER CHECK (assessment >= 0 AND assessment <= 10) NOT NULL
 );
