79d4654bd9f6ef216c9e26be8549beabe9c08419
Ilia Zharinov <19Turboboy90@gmail.com>
2025-04-01 13:28:45 +0300

    Added dependencies for tests

---
+++ src/test/java/ru/zharinov/service/ActorServiceTest.java
@@ -0,0 +1,29 @@
+package ru.zharinov.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ActorServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void findActorById() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findAllActorByMovieId() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findAllActor() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findActorsByPrefix() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void save() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void delete() {
+    }
+}
